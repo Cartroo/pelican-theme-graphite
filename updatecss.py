c372f7acef66cdf2166a5117d2074c823b7e31d2
Andy Pearce <andy@andy-pearce.com>
2016-07-27 21:39:20 +0100

    Added mobile styling
    This is a first stab at an alternative sylesheet which is suitable for
    smaller devices. This allows pages to be reactive and switch to this as
    the window width reduces. This isn't necessarily the most graceful option
    available on modern browsers, but it's a fairly low-hanging fruit and
    doesn't rely on a sharp learning curve in new CSS. I can always improve
    it later to use a somewhat less clumsy approach when I have time.

--- updatecss.py
+++ updatecss.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python
 #
 # Update CSS via Python sass module
+#
+# Note: This script now uses the libsass Python module.
 
+import codecs
 import os
 import sass
 
@@ -12,8 +15,10 @@ for entry in os.listdir("."):
     if ext != ".scss":
         continue
     try:
-        css_content = sass.compile_file(entry, output_style=sass.SASS_STYLE_COMPRESSED)
-        with open(os.path.join("static", base + ".css"), "w") as output_fd:
+        # css_content = sass.compile_file(entry, output_style=sass.SASS_STYLE_COMPRESSED)
+        css_content = sass.compile(filename=entry, output_style='compressed')
+        #with open(os.path.join("static", base + ".css"), "w") as output_fd:
+        with codecs.open(os.path.join("static", base + ".css"),'w',encoding='utf8') as output_fd:
             output_fd.write(css_content)
     except Exception, e:
         print "Error processing %s: %s" % (entry, e)
