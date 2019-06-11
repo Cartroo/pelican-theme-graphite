08fd80cf16aab40a583b253910c9d0cb5275eef6
Andy Pearce <andy@andy-pearce.com>
2019-06-11 13:55:39 +0100

    Compatibility updates
    Primarily compatibility with new versions of Pelican/Jinja which appear
    to have broken the archives pages. Also updated the updatecss.py script
    to be Python 3 compatible.

--- updatecss.py
+++ updatecss.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 #
 # Update CSS via Python sass module
 #
@@ -15,11 +15,11 @@ for entry in os.listdir("."):
     if ext != ".scss":
         continue
     try:
-        css_content = sass.compile_file(entry, output_style=sass.SASS_STYLE_COMPRESSED)
+        css_content = sass.compile_file(entry.encode('utf-8'), output_style=sass.SASS_STYLE_COMPRESSED)
         # css_content = sass.compile(filename=entry, output_style='compressed') # either nested or compressed
         #with open(os.path.join("static", base + ".css"), "w") as output_fd:
         with codecs.open(os.path.join("static", base + ".css"),'w',encoding='utf8') as output_fd:
-            output_fd.write(css_content)
-    except Exception, e:
-        print "Error processing %s: %s" % (entry, e)
+            output_fd.write(css_content.decode('utf-8'))
+    except Exception as e:
+        print("Error processing %s: %s" % (entry, e))
 
